ac1f10c86aebbcb96be9d1063d44d47774ce763a
Tiana Dumitrescu <tiana.dumitrescu@icloud.com>
2026-04-13 19:51:25 -0400

    Added Lucid and Database classes.

--- django_src/main/models.py
+++ django_src/main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from datetime import datetime, timedelta
 
 # Create your models here.
+
+# Alarm Clock Model
 class Alarm(models.Model):
     alarm_time = models.TimeField()
     wake_up_time = models.DateTimeField(null=True, blank=True)
@@ -39,3 +42,31 @@ class Alarm(models.Model):
             return True
         else:
             return False
+
+# Lucid Model - Essentially represents
+class Lucid(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length = 64)
+    # Types represents what "species" is inherited by the Lucids (i.e. )
+    types = ArrayField(models.CharField(max_length = 64), size = 5)
+    description = models.CharField(max_length = 1024)
+    spawn_rate = models.FloatField
+    spawn_level_offset = models.IntegerField
+    # Evolution represents the current level of the Lucid in it's "progression path"
+    # Index one is the previous, index 2 is the next
+    evolution = ArrayField(models.CharField(max_length = 64), size = 2)
+
+# User Database Model - Each user will officially have one
+class UserDatabase(models.Model):
+    # The user's username
+    username = models.CharField(max_length = 64)
+    # The user's saved password
+    password = models.CharField(max_length = 64)
+    # The current score the user has
+    totalPoints = models.IntegerField;
+    # The lucids array essentially holds all the lucids
+    # The number represents the Lucid ID
+    lucids = ArrayField(models.IntegerField, size = 25)
+    # Represents the actual alarm clock model
+    alarm = models.ForeignKey(Alarm, on_delete = models.CASCADE)
+    
